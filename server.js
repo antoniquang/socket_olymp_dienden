81901c366383486c826e483bde7535befab2c256
khanhpqm <khanhpqm@fpt.com.vn>
2021-12-17 00:29:26 +0700

    xx
    xx

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = require('socket.io')(server, {
 server.listen(process.env.PORT || 3001);
 
 
-const url = 'http://api-olymp168.example.com/api/'
+const url = 'hhttps://diendengiadung.com/api_olymp/api/index.php'
 
 //const url = 'http://192.168.100.22/kse_trade/api/' // locals
 const headers = { 'Authorization': 'Basic b2x5bXAxNjhAZ21haWwuY29tOkAxMjNANDU2' }
